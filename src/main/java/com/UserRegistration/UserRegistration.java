f4ed69d2799049738317c6ab51db6e5853ae6e77
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-08 16:02:54 +0530

    Added:Validation For First Name

--- src/main/java/com/UserRegistration/UserRegistration.java
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -1,7 +1,20 @@
 package com.UserRegistration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Process");
+        firstName("Meghnad");
+    }
+    public static void firstName(String firstName) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+        Matcher match = pattern.matcher(firstName);
+        if(match.find() && match.group().equals(firstName)) {
+            System.out.println("valid FirstName");
+        }
+        else {
+            System.out.println("invalid FirstName");
+        }
     }
 }
