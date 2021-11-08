78964eef445ee3ec18aebeefbcadbc840e3e4c6a
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-08 16:56:20 +0530

    Added:Validation For Password containing atleast 1 Number

--- src/main/java/com/UserRegistration/UserRegistration.java
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -11,12 +11,12 @@ public class UserRegistration {
         phoneNumber("919890964596");
         /*
         *User need to  follow pre-defined  Password rules.
-        *Rule2 – minimum 8 Characters and at least 1 should be uppercase
+        *Rule3 – minimum 8 Characters and at least 1 should be number
         */
-        password("passWord");
+        password("passWord3");
     }
     public static void firstName(String firstName) {
-        Pattern pattern = Pattern.compile("^(?=.*[A-Za-z])[A-Za-z]{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
         Matcher match = pattern.matcher(firstName);
         if(match.find() && match.group().equals(firstName)) {
             System.out.println("valid FirstName");
@@ -56,7 +56,7 @@ public class UserRegistration {
         }
     }
     public static void password(String password) {
-        Pattern pattern = Pattern.compile("^(?=.*[A-Za-z])[A-Za-z]{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
         Matcher match = pattern.matcher(password);
         if(match.find() && match.group().equals(password)) {
             System.out.println("valid password");
