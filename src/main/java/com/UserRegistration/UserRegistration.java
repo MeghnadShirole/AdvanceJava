e58363dbb656864096d0c62211ef92e315f23ad9
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-08 19:30:15 +0530

    Added:Validation for Sample Email Ids

--- src/main/java/com/UserRegistration/UserRegistration.java
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -14,6 +14,7 @@ public class UserRegistration {
         *Rule4 – minimum 8 Characters and at least 1 special character
         */
         password("passW@rd3");
+        testForValidEmails();
     }
     public static void firstName(String firstName) {
         Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
@@ -63,6 +64,28 @@ public class UserRegistration {
         }
         else {
             System.out.println("invalid password");
+        }
+        System.out.println("==================");
+    }
+    static String[] validEmails = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+            "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+            "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc",
+            "abc@.com.my","abc123@gmail.a", "abc123@.com",
+            "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com",
+            "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
+            "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+    public static void testForValidEmails() {
+        System.out.println("EmailId Check\n");
+        for (int i = 0; i < validEmails.length; i++) {
+            Pattern pattern = Pattern.compile("^[a-zA-Z-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$");
+            Matcher match = pattern.matcher(validEmails[i]);
+            if(match.matches())
+            {
+                System.out.println(validEmails[i]+" --> Is Valid");
+            } else {
+                System.out.println(validEmails[i]+" --> Is Invalid");
+            }
         }
     }
 }
